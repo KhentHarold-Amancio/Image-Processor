94236d9cb97c3fec059daa8c341bd70aa41ac3c8
Khent Harold Amancio <76564829+KhentHarold-Amancio@users.noreply.github.com>
2023-11-27 08:07:09 +0800

    Add project files.

---
+++ Image Processor/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Image_Processor
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            menuStrip1 = new MenuStrip();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem1 = new ToolStripMenuItem();
+            button2 = new Button();
+            button3 = new Button();
+            textBox1 = new TextBox();
+            label5 = new Label();
+            pictureBox2 = new PictureBox();
+            histogramPanel = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Location = new Point(56, 93);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(422, 232);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(56, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Original Image";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(539, 64);
+            label2.Name = "label2";
+            label2.Size = new Size(63, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Histogram";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Basic Copy", "Grayscale", "Color Inversion", "Sepia" });
+            comboBox1.Location = new Point(614, 490);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(191, 23);
+            comboBox1.TabIndex = 4;
+            comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(515, 493);
+            label3.Name = "label3";
+            label3.Size = new Size(93, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Choose process:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(56, 349);
+            label4.Name = "label4";
+            label4.Size = new Size(96, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Processed Image";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(730, 530);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 8;
+            button1.Text = "Process";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1021, 24);
+            menuStrip1.TabIndex = 9;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, exitToolStripMenuItem, exitToolStripMenuItem1 });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(103, 22);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(103, 22);
+            exitToolStripMenuItem.Text = "Save";
+            // 
+            // exitToolStripMenuItem1
+            // 
+            exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
+            exitToolStripMenuItem1.Size = new Size(103, 22);
+            exitToolStripMenuItem1.Text = "Exit";
+            exitToolStripMenuItem1.Click += exitToolStripMenuItem1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(730, 559);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 10;
+            button2.Text = "Save";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(515, 413);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 11;
+            button3.Text = "Upload";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(596, 413);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(209, 23);
+            textBox1.TabIndex = 12;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(596, 395);
+            label5.Name = "label5";
+            label5.Size = new Size(58, 15);
+            label5.TabIndex = 13;
+            label5.Text = "File name";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox2.Location = new Point(56, 378);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(422, 232);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 14;
+            pictureBox2.TabStop = false;
+            // 
+            // histogramPanel
+            // 
+            histogramPanel.BorderStyle = BorderStyle.FixedSingle;
+            histogramPanel.Location = new Point(539, 93);
+            histogramPanel.Name = "histogramPanel";
+            histogramPanel.Size = new Size(256, 200);
+            histogramPanel.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1021, 636);
+            Controls.Add(histogramPanel);
+            Controls.Add(pictureBox2);
+            Controls.Add(label5);
+            Controls.Add(textBox1);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(pictureBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Basic Image Processor";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private Label label1;
+        private Label label2;
+        private ComboBox comboBox1;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem1;
+        private Button button2;
+        private Button button3;
+        private TextBox textBox1;
+        private Label label5;
+        private PictureBox pictureBox2;
+        private Panel histogramPanel;
+    }
+}
---
+++ Image Processor/Form1.cs
@@ -0,0 +1,313 @@
+using System.Diagnostics.Metrics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace Image_Processor
+{
+    public partial class Form1 : Form
+    {
+
+        int[] histogram;
+        string imagePath;
+        Bitmap image;
+        Bitmap processedImage;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            textBox1.Enabled = false;
+        }
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.bmp;*.jpg;*.png;*.gif;*.tif|All files|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Load the selected image
+                    imagePath = openFileDialog.FileName;
+                    string file = Path.GetFileName(imagePath);
+                    textBox1.Text = file;
+                    Bitmap image = new Bitmap(imagePath);
+                    ComputeHistogram(image);
+                    drawHistogram();
+                    pictureBox1.Image = image;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void exitToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.bmp;*.jpg;*.png;*.gif;*.tif|All files|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    // Load the selected image
+                    imagePath = openFileDialog.FileName;
+                    string file = Path.GetFileName(imagePath);
+                    textBox1.Text = file;
+                    Bitmap image = new Bitmap(imagePath);
+                    ComputeHistogram(image);
+                    drawHistogram();
+                    pictureBox1.Image = image;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        //COMPUT AND STORE THE HISTOGRAM DATA ON AN INT ARRAY
+        private void ComputeHistogram(Bitmap image)
+        {
+            histogram = new int[256];
+
+            for (int x = 0; x < image.Width; x++)
+            {
+                for (int y = 0; y < image.Height; y++)
+                {
+                    Color pixelColor = image.GetPixel(x, y);
+                    int grayValue = (int)(pixelColor.R * 0.3 + pixelColor.G * 0.59 + pixelColor.B * 0.11);
+                    histogram[grayValue]++;
+                }
+            }
+        }
+        //INPUT THE HISTOGRAM ON THE PANEL
+        private void drawHistogram()
+        {
+            if (histogramPanel != null)
+            {
+                using (Graphics g = histogramPanel.CreateGraphics())
+                {
+                    int barWidth = histogramPanel.Width / histogram.Length;
+
+                    for (int i = 0; i < histogram.Length; i++)
+                    {
+                        int barHeight = histogram[i];
+                        Rectangle barRect = new Rectangle(i * barWidth, histogramPanel.Height - barHeight, barWidth, barHeight);
+                        g.FillRectangle(Brushes.Gray, barRect);
+                    }
+                }
+            }
+        }
+        //BASIC COPY
+        private void basicCopy()
+        {
+            try
+            {
+                Bitmap sourceImage = new Bitmap(imagePath);
+                Bitmap copiedImage = new Bitmap(sourceImage.Width,
+                                                sourceImage.Height,
+                                                sourceImage.PixelFormat);
+
+                for (int x = 0; x < sourceImage.Width; x++)
+                {
+                    for (int y = 0; y < sourceImage.Height; y++)
+                    {
+                        Color pixelColor = sourceImage.GetPixel(x, y);
+                        copiedImage.SetPixel(x, y, pixelColor);
+                    }
+                }
+
+                pictureBox2.Image = copiedImage;
+                processedImage = copiedImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        //GRAYSCALE EFFECT
+        private void grayScale()
+        {
+            try
+            {
+                Bitmap sourceImage = new Bitmap(imagePath);
+                Bitmap grayscaleImage = new Bitmap(sourceImage.Width,
+                                                sourceImage.Height,
+                                                sourceImage.PixelFormat);
+
+                for (int x = 0; x < sourceImage.Width; x++)
+                {
+                    for (int y = 0; y < sourceImage.Height; y++)
+                    {
+                        Color pixelColor = sourceImage.GetPixel(x, y);
+                        int grayValue = (int)(pixelColor.R * 0.3 + pixelColor.G * 0.59 + pixelColor.B * 0.11);
+                        grayscaleImage.SetPixel(x, y, Color.FromArgb(grayValue, grayValue, grayValue));
+                    }
+                }
+                pictureBox2.Image = grayscaleImage;
+                processedImage = grayscaleImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        //COLOR INVERSION
+        private void colorInversion()
+        {
+            try
+            {
+                Bitmap sourceImage = new Bitmap(imagePath);
+                Bitmap colorInverted = new Bitmap(sourceImage.Width,
+                                                sourceImage.Height,
+                                                sourceImage.PixelFormat);
+
+                for (int x = 0; x < sourceImage.Width; x++)
+                {
+                    for (int y = 0; y < sourceImage.Height; y++)
+                    {
+                        Color pixelColor = sourceImage.GetPixel(x, y);
+
+                        int invertedR = 255 - pixelColor.R;
+                        int invertedG = 255 - pixelColor.G;
+                        int invertedB = 255 - pixelColor.B;
+
+                        invertedR = Math.Max(0, Math.Min(255, invertedR));
+                        invertedG = Math.Max(0, Math.Min(255, invertedG));
+                        invertedB = Math.Max(0, Math.Min(255, invertedB));
+
+                        colorInverted.SetPixel(x, y, Color.FromArgb(invertedR, invertedG, invertedB));
+                    }
+                }
+
+                pictureBox2.Image = colorInverted;
+                processedImage = colorInverted;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        //SEPIA AN IMAGE
+        private void sepiaImage()
+        {
+            try
+            {
+                Bitmap sourceImage = new Bitmap(imagePath);
+                Bitmap sepiaImage = new Bitmap(sourceImage.Width,
+                                                sourceImage.Height,
+                                                sourceImage.PixelFormat);
+
+                for (int x = 0; x < sourceImage.Width; x++)
+                {
+                    for (int y = 0; y < sourceImage.Height; y++)
+                    {
+                        Color pixelColor = sourceImage.GetPixel(x, y);
+
+                        int sepiaR = (int)(0.393 * pixelColor.R + 0.769 * pixelColor.G + 0.189 * pixelColor.B);
+                        int sepiaG = (int)(0.349 * pixelColor.R + 0.686 * pixelColor.G + 0.168 * pixelColor.B);
+                        int sepiaB = (int)(0.272 * pixelColor.R + 0.534 * pixelColor.G + 0.131 * pixelColor.B);
+
+                        sepiaR = Math.Max(0, Math.Min(255, sepiaR));
+                        sepiaG = Math.Max(0, Math.Min(255, sepiaG));
+                        sepiaB = Math.Max(0, Math.Min(255, sepiaB));
+
+                        sepiaImage.SetPixel(x, y, Color.FromArgb(sepiaR, sepiaG, sepiaB));
+                    }
+                }
+
+                pictureBox2.Image = sepiaImage;
+                processedImage = sepiaImage;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ComputeHistogram(image);
+            drawHistogram();
+        }
+        
+        //PROCESS BUTTON
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    basicCopy();
+                    break;
+                case 1:
+                    grayScale();
+                    break;
+                case 2:
+                    colorInversion();
+                    break;
+                case 3:
+                    sepiaImage();
+                    break;
+
+            }
+        }
+        //SAVE BUTTON
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "JPEG Image|*.jpg|PNG Image|*.png|Bitmap Image|*.bmp|GIF Image|*.gif|TIFF Image|*.tif";
+            saveFileDialog.Title = "Save Image";
+            saveFileDialog.FileName = "image";  // Default file name
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Get the selected file name and extension
+                string fileName = saveFileDialog.FileName;
+
+                // Determine the format based on the selected filter
+                ImageFormat imageFormat = ImageFormat.Jpeg;  // Default to JPEG
+                string ext = System.IO.Path.GetExtension(fileName);
+                if (ext.Equals(".png", StringComparison.OrdinalIgnoreCase))
+                    imageFormat = ImageFormat.Png;
+                else if (ext.Equals(".bmp", StringComparison.OrdinalIgnoreCase))
+                    imageFormat = ImageFormat.Bmp;
+                else if (ext.Equals(".gif", StringComparison.OrdinalIgnoreCase))
+                    imageFormat = ImageFormat.Gif;
+                else if (ext.Equals(".tif", StringComparison.OrdinalIgnoreCase) || ext.Equals(".tiff", StringComparison.OrdinalIgnoreCase))
+                    imageFormat = ImageFormat.Tiff;
+
+                try
+                {
+                    // Save the Bitmap to the selected file
+                    processedImage.Save(fileName, imageFormat);
+
+                    MessageBox.Show("Image saved successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error saving image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
